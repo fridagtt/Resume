74fdce513373b58ccd9edcb341f1a71ea4d07096
Fridagtt <fridagtzm@gmail.com>
2020-06-24 17:36:16 -0500

    Add AboutMe section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { AchievementsComponent } from './achievements/achievements.component';
-import { ContactComponent } from './contact/contact.component';
 import { EducationComponent } from './education/education.component';
 import { LeadershipsComponent } from './leaderships/leaderships.component';
 import { ProjectsComponent } from './projects/projects.component';
@@ -14,7 +13,6 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'achievements', component: AchievementsComponent },
-  { path: 'contact', component: ContactComponent },
   { path: 'education', component: EducationComponent },
   { path: 'leaderships', component: LeadershipsComponent },
   { path: 'projects', component: ProjectsComponent },
