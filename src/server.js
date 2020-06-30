894ae1ef30927badfe6fabb6eae53d1560db956b
Fridagtt <fridagtzm@gmail.com>
2020-06-30 14:53:02 -0500

    Changes to server.js document

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+app.use(express.static(__dirname+'/dist/resume-frida'));
+app.get('/*',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/resume-frida/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
