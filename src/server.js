d701e1ff177713bfda0be57bfc8ecba6af8d1ccd
Fridagtt <fridagtzm@gmail.com>
2020-06-30 14:57:27 -0500

    Changes to server.js document on name

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/resume-frida'));
+app.use(express.static(__dirname+'/dist/resume'));
 app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/resume-frida/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/resume/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
